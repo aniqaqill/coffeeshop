f80d7e2bc66707c6a860a6619a6fe037e679faa5
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-03-06 03:24:43 +0800

    Create First Version Model

--- projectcoffee/coffeeshoplist/models.py
+++ projectcoffee/coffeeshoplist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.\
+class CoffeeShop(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    hours = models.CharField(max_length=100)
+class Rating(models.Model):
+    coffee_shop = models.ForeignKey(CoffeeShop, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.TextField(blank=True)
