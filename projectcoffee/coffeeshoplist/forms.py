90685e4733a119420ed4e6ea92d7ee610a5abac9
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-03-06 17:15:33 +0800

    setbacks for ratings

--- projectcoffee/coffeeshoplist/forms.py
+++ projectcoffee/coffeeshoplist/forms.py
@@ -1,12 +1,8 @@
 from django import forms
 from .models import Rating
-from django_star_ratings.widgets import StarRatingInput
-
 
 class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['rating', 'review']
-        widgets = {
-            'rating': StarRatingInput(),
-        }
+ 
