c12810a3de129c8afe2b2901e124e4a91136752e
Aniq Aqil <65521085+aniqaqill@users.noreply.github.com>
2023-03-08 05:11:48 +0800

    Delete projectcoffee directory

--- projectcoffee/coffeeshoplist/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import CoffeeShop, Rating
-
-admin.site.register(CoffeeShop)
-admin.site.register(Rating)
--- projectcoffee/coffeeshoplist/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoffeeshoplistConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'coffeeshoplist'
